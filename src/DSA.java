f010efcc53ddd776432bf8143ce43029407d2f71
Manish Chauhan <manish.fastprep@gmail.com>
2023-06-17 19:58:45 +0530

    Added the stream methods for linear search

--- src/DSA.java
+++ src/DSA.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class DSA
 {
@@ -8,6 +12,10 @@ public class DSA
 		int[] arr1 = {18,12,9,14,77,50};
 		int linearSearchResult =obj.linearSearch(new int[] {18,12,9,14,77,50},14);
 		System.out.println(linearSearchResult);
+		int linearSearchStreamResult =obj.linearSearchStream(new int[] {18,12,9,14,77,50},14);
+		System.out.println(linearSearchStreamResult);
+		int linearSearchStreamWitoutBoxingResult =obj.linearSearchStreamWitoutBoxing(new int[] {18,12,9,14,77,50},14);
+		System.out.println(linearSearchStreamWitoutBoxingResult);
 
 	}
 	/**
@@ -28,5 +36,28 @@ public class DSA
 		
 		return -1;
 	}
+	/** To find time complexity , find the relation from the graph  between 
+	 * the size of the input   vs max number of operations 
+	 * Since the graph will be linear the time complexity is O(n) 
+	 * */
+	public int linearSearchStream(int[] arr, int target) {
+	    if (arr == null || arr.length == 0)
+	        return -1;
+	    
+	    return Arrays.stream(arr)	// return IntStream 
+	    		.boxed()			// converts to Stream<Integer>
+	    		.collect(Collectors.toList()) // collects to List 
+	    		.indexOf(target); // returns index of target . return -1 if not found
+	}
+	
+	public int linearSearchStreamWitoutBoxing(int[] arr, int target) {
+	    if (arr == null || arr.length == 0)
+	        return -1;
 
+	   OptionalInt index = IntStream.range(0, arr.length) // create a range of numbers starting from 0 and excluding arr.length
+			   .filter(pivot ->arr[pivot]==target) // filter for the condition
+			   .findFirst(); // returns first element . This is a short circuit operation
+	   
+	   return index.orElse(-1);
+	}
 }
