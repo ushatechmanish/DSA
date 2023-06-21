d6d2de8a76253eda9aeaa36c8b1a8839e90efb2d
Manish Chauhan <manish.fastprep@gmail.com>
2023-06-21 21:06:29 +0530

    Arrays Method and Random class methods closes #36

---
+++ src/goodrich/arrays/ch3/RandomNumberGenerator.java
@@ -0,0 +1,41 @@
+package goodrich.arrays.ch3;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class RandomNumberGenerator
+{
+	public static void main(String[] args)
+	{
+		RandomNumberGenerator obj = new RandomNumberGenerator();
+		Random rand = new Random(50);
+		Random rand2 = new Random(System.currentTimeMillis());
+		// this will always create a new sequence every time program is run
+		System.out.println(rand.nextInt(0, 10));
+		IntStream.range(0, 10).mapToObj(i -> obj.getRandomArrays(0, 8))
+				.forEach(var -> System.out.println(Arrays.toString(var)));
+		/**
+		 * 50 is the seed . This generates pseudoRandom Number
+		 * 
+		 * A formula which can be used for creating a pseudoRandom number is
+		 * 
+		 * next = (a*cur +b)% n ; where a, b , n can be chosen .
+		 * 
+		 * For Math.random n value is taken 2^48
+		 */
+		System.out.println(rand.nextInt(10)); // this will not give 10 in any case . 10 is excluded
+		rand.nextBoolean();// this will give random boolean value;
+	}
+
+	public int[] getRandomArrays(int startSize, int endSize)
+	{
+		int size = new Random(System.currentTimeMillis()).nextInt(startSize, endSize);
+		int[] result = new int[size];
+		for (int i = 0; i < size; ++i)
+		{
+			result[i] = -10 + (int) (Math.random() * 10);
+		}
+		return result;
+	}
+}
--- src/test/UnitTests.java
+++ src/test/UnitTests.java
@@ -10,71 +10,100 @@ import org.junit.jupiter.api.BeforeEach;
 import binarysearch.BinarySearch;
 import goodrich.arrays.ch3.GameEntry;
 import goodrich.arrays.ch3.InsertionSort;
+import goodrich.arrays.ch3.RandomNumberGenerator;
 import goodrich.arrays.ch3.Scoreboard;
 
 public class UnitTests
 {
 	private BinarySearch binarySearch;
-	int[] emptyArr = {};
-	int[] arr2 = {1,4,7,9,20};
-	int[] arr3 = {1,4,7,9,20};
-	int[] arr4 = {1,4,7,9,20};
-	
+	RandomNumberGenerator randomNumberUtility;
+	int[] emptyArr =
+	{};
+	int[] arr2 =
+	{ 1, 4, 7, 9, 20 };
+	int[] arr3 =
+	{ 1, 4, 7, 9, 20 };
+	int[] arr4 =
+	{ 1, 4, 7, 9, 20 };
+
 	@BeforeEach
 	public void setup()
 	{
-		binarySearch=new BinarySearch();
-		
+		binarySearch = new BinarySearch();
+		randomNumberUtility = new RandomNumberGenerator();
 	}
+
 	@Test
 	public void testGameEntry()
 	{
 		Scoreboard board = new Scoreboard(4);
 		board.addEntry(new GameEntry("manish", 5));
-		board.addEntry(new GameEntry("vineet", 10 ));
-		board.addEntry(new GameEntry("vineet", 6 ));
-		board.addEntry(new GameEntry("vineet", 4 ));
-		board.addEntry(new GameEntry("vineet", -1 ));		
-		assertEquals("[10, 6, 5, 4]",board.toString());
-		board.removeEntry(new GameEntry("vineet", 10 ));
-		assertEquals("[6, 5, 4, null]",board.toString());
+		board.addEntry(new GameEntry("vineet", 10));
+		board.addEntry(new GameEntry("vineet", 6));
+		board.addEntry(new GameEntry("vineet", 4));
+		board.addEntry(new GameEntry("vineet", -1));
+		assertEquals("[10, 6, 5, 4]", board.toString());
+		board.removeEntry(new GameEntry("vineet", 10));
+		assertEquals("[6, 5, 4, null]", board.toString());
 	}
+
 	@Test
 	public void testInsertionSort()
 	{
-		InsertionSort obj = new InsertionSort();
-		assertEquals("[1, 2]",Arrays.toString(obj.sortAscending1(new int[] {2,1})) );
-		assertEquals("[]",Arrays.toString(obj.sortAscending1(new int[] {})) );
-		assertEquals("[1]",Arrays.toString(obj.sortAscending1(new int[] {1})) );
-		assertEquals("[1, 1, 2, 3, 3, 4, 5]",Arrays.toString(obj.sortAscending1(new int[] {1,3,4,5,1,2,3})) );
+		InsertionSort insertionSort = new InsertionSort();
+		assertEquals("[1, 2]", Arrays.toString(insertionSort.sortAscending1(new int[]
+		{ 2, 1 })));
+		assertEquals("[]", Arrays.toString(insertionSort.sortAscending1(new int[]
+		{})));
+		assertEquals("[1]", Arrays.toString(insertionSort.sortAscending1(new int[]
+		{ 1 })));
+		assertEquals("[1, 1, 2, 3, 3, 4, 5]", Arrays.toString(insertionSort.sortAscending1(new int[]
+		{ 1, 3, 4, 5, 1, 2, 3 })));
 	}
+
 	@Test
 	public void testInsertionSortNew()
 	{
 		InsertionSort obj = new InsertionSort();
-		assertEquals("[1, 2]",Arrays.toString(obj.sortAscending2(new int[] {2,1})) );
-		assertEquals("[]",Arrays.toString(obj.sortAscending2(new int[] {})) );
-		assertEquals("[1]",Arrays.toString(obj.sortAscending2(new int[] {1})) );
-		assertEquals("[1, 1, 2, 3, 3, 4, 5]",Arrays.toString(obj.sortAscending2(new int[] {1,3,4,5,1,2,3})) );
+		assertEquals("[1, 2]", Arrays.toString(obj.sortAscending2(new int[]
+		{ 2, 1 })));
+		assertEquals("[]", Arrays.toString(obj.sortAscending2(new int[]
+		{})));
+		assertEquals("[1]", Arrays.toString(obj.sortAscending2(new int[]
+		{ 1 })));
+		assertEquals("[1, 1, 2, 3, 3, 4, 5]", Arrays.toString(obj.sortAscending2(new int[]
+		{ 1, 3, 4, 5, 1, 2, 3 })));
 	}
+
 	@Test
 	public void testInsertionSortDesc1()
 	{
 		InsertionSort obj = new InsertionSort();
-		assertEquals("[2, 1]",Arrays.toString(obj.sortDescending1(new int[] {2,1})) );
-		assertEquals("[]",Arrays.toString(obj.sortDescending1(new int[] {})) );
-		assertEquals("[1]",Arrays.toString(obj.sortDescending1(new int[] {1})) );
-		assertEquals("[5, 4, 3, 3, 2, 1, 1]",Arrays.toString(obj.sortDescending1(new int[] {1,3,4,5,1,2,3})) );
-		assertEquals("[5, 4, 3]",Arrays.toString(obj.sortDescending1(new int[] {3,4,5})) );
+		assertEquals("[2, 1]", Arrays.toString(obj.sortDescending1(new int[]
+		{ 2, 1 })));
+		assertEquals("[]", Arrays.toString(obj.sortDescending1(new int[]
+		{})));
+		assertEquals("[1]", Arrays.toString(obj.sortDescending1(new int[]
+		{ 1 })));
+		assertEquals("[5, 4, 3, 3, 2, 1, 1]", Arrays.toString(obj.sortDescending1(new int[]
+		{ 1, 3, 4, 5, 1, 2, 3 })));
+		assertEquals("[5, 4, 3]", Arrays.toString(obj.sortDescending1(new int[]
+		{ 3, 4, 5 })));
 	}
+
 	@Test
 	public void testInsertionSortDesc2()
 	{
 		InsertionSort obj = new InsertionSort();
-		assertEquals("[2, 1]",Arrays.toString(obj.sortDescending2(new int[] {2,1})) );
-		assertEquals("[]",Arrays.toString(obj.sortDescending2(new int[] {})) );
-		assertEquals("[1]",Arrays.toString(obj.sortDescending2(new int[] {1})) );
-		assertEquals("[5, 4, 3, 3, 2, 1, 1]",Arrays.toString(obj.sortDescending2(new int[] {1,3,4,5,1,2,3})) );
-		assertEquals("[5, 4, 3]",Arrays.toString(obj.sortDescending2(new int[] {3,4,5})) );
+		assertEquals("[2, 1]", Arrays.toString(obj.sortDescending2(new int[]
+		{ 2, 1 })));
+		assertEquals("[]", Arrays.toString(obj.sortDescending2(new int[]
+		{})));
+		assertEquals("[1]", Arrays.toString(obj.sortDescending2(new int[]
+		{ 1 })));
+		assertEquals("[5, 4, 3, 3, 2, 1, 1]", Arrays.toString(obj.sortDescending2(new int[]
+		{ 1, 3, 4, 5, 1, 2, 3 })));
+		assertEquals("[5, 4, 3]", Arrays.toString(obj.sortDescending2(new int[]
+		{ 3, 4, 5 })));
 	}
 }
