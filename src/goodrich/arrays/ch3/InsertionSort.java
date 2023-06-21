8368ab45ed04ae1d0d85fe94bdf4274f5260055a
Manish Chauhan <manish.fastprep@gmail.com>
2023-06-21 13:08:54 +0530

    Added methods for insertion sort for descending order and included the unit tests also. closes #34

--- src/goodrich/arrays/ch3/InsertionSort.java
+++ src/goodrich/arrays/ch3/InsertionSort.java
@@ -3,8 +3,51 @@ package goodrich.arrays.ch3;
 public class InsertionSort
 {
 
+	public int[] sortAscending2(int[] arr)
+	{
+		for(int i=1 ; i<arr.length ; ++i)
+		{
+			int j=i;
+			int curr= arr[i];
+			while(j>0 && arr[j-1]>curr)
+			{
+				// shift the elements 
+				arr[j]=arr[j-1];
+				--j;
+			}
+			arr[j]=curr;
+		}
+		return arr;
+	}
+	public int[] sortDescending1(int[] arr) //[2,4,3,5]
+	{
+		for(int i=1 ; i<arr.length ; ++i)
+		{
+			int j=i;
+			int curr= arr[i];
+			while(j>0 && arr[j-1]<curr)
+			{
+				// shift the elements 
+				arr[j]=arr[j-1];
+				--j;
+			}
+			arr[j]=curr;
+		}
+		return arr;
+	}
+	public int[] sortDescending2(int[] arr) //[2,4,3,5]
+	{
+		for(int i=0 ; i<arr.length-1 ; ++i)
+		{
+			for(int j=i+1; j>0 && arr[j]>arr[j-1] ;--j)
+			{
+				swap(arr, j, j-1);
+			}
+		}
+		return arr;
+	}
 	// sort the array in ascending order
-	public int[] sort(int[] arr)
+	public int[] sortAscending1(int[] arr)
 	{
 		// intuition . The array is divided into left part and right part excluding the current index position
 		// Left part is always sorted 
--- src/test/UnitTests.java
+++ src/test/UnitTests.java
@@ -43,9 +43,38 @@ public class UnitTests
 	public void testInsertionSort()
 	{
 		InsertionSort obj = new InsertionSort();
-		assertEquals("[1, 2]",Arrays.toString(obj.sort(new int[] {2,1})) );
-		assertEquals("[]",Arrays.toString(obj.sort(new int[] {})) );
-		assertEquals("[1]",Arrays.toString(obj.sort(new int[] {1})) );
-		assertEquals("[1, 1, 2, 3, 3, 4, 5]",Arrays.toString(obj.sort(new int[] {1,3,4,5,1,2,3})) );
+		assertEquals("[1, 2]",Arrays.toString(obj.sortAscending1(new int[] {2,1})) );
+		assertEquals("[]",Arrays.toString(obj.sortAscending1(new int[] {})) );
+		assertEquals("[1]",Arrays.toString(obj.sortAscending1(new int[] {1})) );
+		assertEquals("[1, 1, 2, 3, 3, 4, 5]",Arrays.toString(obj.sortAscending1(new int[] {1,3,4,5,1,2,3})) );
+	}
+	@Test
+	public void testInsertionSortNew()
+	{
+		InsertionSort obj = new InsertionSort();
+		assertEquals("[1, 2]",Arrays.toString(obj.sortAscending2(new int[] {2,1})) );
+		assertEquals("[]",Arrays.toString(obj.sortAscending2(new int[] {})) );
+		assertEquals("[1]",Arrays.toString(obj.sortAscending2(new int[] {1})) );
+		assertEquals("[1, 1, 2, 3, 3, 4, 5]",Arrays.toString(obj.sortAscending2(new int[] {1,3,4,5,1,2,3})) );
+	}
+	@Test
+	public void testInsertionSortDesc1()
+	{
+		InsertionSort obj = new InsertionSort();
+		assertEquals("[2, 1]",Arrays.toString(obj.sortDescending1(new int[] {2,1})) );
+		assertEquals("[]",Arrays.toString(obj.sortDescending1(new int[] {})) );
+		assertEquals("[1]",Arrays.toString(obj.sortDescending1(new int[] {1})) );
+		assertEquals("[5, 4, 3, 3, 2, 1, 1]",Arrays.toString(obj.sortDescending1(new int[] {1,3,4,5,1,2,3})) );
+		assertEquals("[5, 4, 3]",Arrays.toString(obj.sortDescending1(new int[] {3,4,5})) );
+	}
+	@Test
+	public void testInsertionSortDesc2()
+	{
+		InsertionSort obj = new InsertionSort();
+		assertEquals("[2, 1]",Arrays.toString(obj.sortDescending2(new int[] {2,1})) );
+		assertEquals("[]",Arrays.toString(obj.sortDescending2(new int[] {})) );
+		assertEquals("[1]",Arrays.toString(obj.sortDescending2(new int[] {1})) );
+		assertEquals("[5, 4, 3, 3, 2, 1, 1]",Arrays.toString(obj.sortDescending2(new int[] {1,3,4,5,1,2,3})) );
+		assertEquals("[5, 4, 3]",Arrays.toString(obj.sortDescending2(new int[] {3,4,5})) );
 	}
 }
